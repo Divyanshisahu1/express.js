6df44d76cf48d0f7a54646a2ef0b5b08d1bce548
Divyanshisahu01 <divyanshi.sahu@wehealapp.in>
2022-12-20 20:32:45 +0530

    to serve static page in express

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
+const path = require("path")
 const app = express()
+console.log(__dirname)
+
+let publicPath = path.join(__dirname,"../public")
+console.log(publicPath)
+
 
 app.get("/", (req, res)=>{
     res.send("hello from the express")
@@ -8,6 +14,9 @@ app.get("/", (req, res)=>{
 app.get("/about", (req,res)=>{
     res.send("hello from the about page")
 })
+
+//built in middleware
+app.use(express.static(publicPath))  //to serve the static page 
 
 app.listen(8000,()=>{
     console.log("listening to port 8000")
